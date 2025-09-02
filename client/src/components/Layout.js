89ec076b2991007ed866d5f6a0aae9b6e80cdae4
simbayippy <simbayippy@gmail.com>
2025-09-02 10:36:19 +0800

    finish lab

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import Footer from './Footer';
-import Header from './Header';
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
 import { Helmet } from "react-helmet";
-import  { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
-
-const Layout = ({ children, title, description, keywords, author }) => {
+const Layout = ({
+  children,
+  title = "Ecommerce app - shop now",
+  description = "mern stack project",
+  keywords = "mern,react,node,mongodb",
+  author = "Techinfoyt",
+}) => {
   return (
     <div>
       <Helmet>
@@ -23,13 +28,6 @@ const Layout = ({ children, title, description, keywords, author }) => {
       <Footer />
     </div>
   );
-};
-
-Layout.defaultProps = {
-  title: "Ecommerce app - shop now",
-  description: "mern stack project",
-  keywords: "mern,react,node,mongodb",
-  author: "Techinfoyt",
 };
 
 export default Layout;
