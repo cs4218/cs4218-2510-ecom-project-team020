4138efb35579ee580dcf22ae4cd51f7138555401
Ritika Joshi <ritikajoshi0114@gmail.com>
2025-09-19 15:05:36 +0800

    bug-fix: fix component to handle empty localStorage safely

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -39,8 +39,13 @@ const Profile = () => {
         toast.error(data?.error);
       } else {
         setAuth({ ...auth, user: data?.updatedUser });
-        let ls = localStorage.getItem("auth");
-        ls = JSON.parse(ls);
+        // let ls = localStorage.getItem("auth");
+        // ls = JSON.parse(ls);
+        // ls.user = data.updatedUser;
+        // localStorage.setItem("auth", JSON.stringify(ls));
+        let ls = localStorage.getItem("auth")
+          ? JSON.parse(localStorage.getItem("auth"))
+          : { token: "", user: {} };
         ls.user = data.updatedUser;
         localStorage.setItem("auth", JSON.stringify(ls));
         toast.success("Profile Updated Successfully");
