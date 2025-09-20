d58e1ffd5ea0f1a29e5b7a08c45be789dbefd3fe
Ritika Joshi <ritikajoshi0114@gmail.com>
2025-09-20 17:50:39 +0800

    bug-fix: improve components to have labels for less brittle test cases

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
+
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -14,7 +15,6 @@ const Register = () => {
   const [answer, setAnswer] = useState("");
   const navigate = useNavigate();
 
-  // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -44,84 +44,99 @@ const Register = () => {
       <div className="form-container" style={{ minHeight: "90vh" }}>
         <form onSubmit={handleSubmit}>
           <h4 className="title">REGISTER FORM</h4>
+
           <div className="mb-3">
             <input
               type="text"
+              id="exampleInputName1"
+              aria-label="name"
+              placeholder="Enter Your Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
               className="form-control"
-              id="exampleInputName1"
-              placeholder="Enter Your Name"
               required
               autoFocus
             />
           </div>
+
           <div className="mb-3">
             <input
               type="email"
+              id="exampleInputEmail1"
+              aria-label="email"
+              placeholder="Enter Your Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
               required
             />
           </div>
+
           <div className="mb-3">
             <input
               type="password"
+              id="exampleInputPassword1"
+              aria-label="password"
+              placeholder="Enter Your Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your Password"
               required
             />
           </div>
+
           <div className="mb-3">
             <input
               type="text"
+              id="exampleInputPhone1"
+              aria-label="phone"
+              placeholder="Enter Your Phone"
               value={phone}
               onChange={(e) => setPhone(e.target.value)}
               className="form-control"
-              id="exampleInputPhone1"
-              placeholder="Enter Your Phone"
               required
             />
           </div>
+
           <div className="mb-3">
             <input
               type="text"
+              id="exampleInputaddress1"
+              aria-label="address"
+              placeholder="Enter Your Address"
               value={address}
               onChange={(e) => setAddress(e.target.value)}
               className="form-control"
-              id="exampleInputaddress1"
-              placeholder="Enter Your Address"
               required
             />
           </div>
+
           <div className="mb-3">
             <input
-              type="Date"
+              type="date"
+              id="exampleInputDOB1"
+              aria-label="dob"
+              placeholder="Enter Your DOB"
               value={DOB}
               onChange={(e) => setDOB(e.target.value)}
               className="form-control"
-              id="exampleInputDOB1"
-              placeholder="Enter Your DOB"
               required
             />
           </div>
+
           <div className="mb-3">
             <input
               type="text"
+              id="exampleInputanswer1"
+              aria-label="answer"
+              placeholder="What is Your Favorite sports"
               value={answer}
               onChange={(e) => setAnswer(e.target.value)}
               className="form-control"
-              id="exampleInputanswer1"
-              placeholder="What is Your Favorite sports"
               required
             />
           </div>
+
           <button type="submit" className="btn btn-primary">
             REGISTER
           </button>
