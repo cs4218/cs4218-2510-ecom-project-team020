b6bae40b53b1a36f21cdff7a1fbbb219932dd1b7
Ritika Joshi <ritikajoshi0114@gmail.com>
2025-10-02 20:23:44 +0800

    bug-fix: fix lack of error response and add validation for user existence

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -12,27 +12,46 @@ export const requireSignIn = async (req, res, next) => {
         next();
     } catch (error) {
         console.log(error);
+        return res.status(401).send({
+            success: false,
+            message: "Unauthorized Access",
+        });
     }
 };
 
 //admin access
 export const isAdmin = async (req, res, next) => {
     try {
-        const user = await userModel.findById(req.user._id);
-        if(user.role !== 1) {
+        if (!req.user?._id) {
             return res.status(401).send({
                 success: false,
                 message: "Unauthorized Access",
+            });
+        }
+
+        const user = await userModel.findById(req.user._id);
+
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        if (user.role !== 1) {
+            return res.status(403).send({
+                success: false,
+                message: "Forbidden",
             });
         } else {
             next();
         }
     } catch (error) {
         console.log(error);
-        res.status(401).send({
+        res.status(500).send({
             success: false,
             error,
-            message: "Something went wrong", // TODO: add this to security risk so make errors generic
+            message: "Something went wrong",
         });
     }
 };
