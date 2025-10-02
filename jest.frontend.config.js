6c781e94587cb749df74f92b8a028249e3b235ac
Gerald Ng Jun Xiang <geraldngjx@gmail.com>
2025-10-02 20:08:24 +0800

    feat: update jest fe config to run component unit tests automatically

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -23,13 +23,15 @@ export default {
   testMatch: [
     "<rootDir>/client/src/pages/**/*.test.js",
     "<rootDir>/client/src/hooks/**/*.test.js",
+    "<rootDir>/client/src/components/**/*.test.js",
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
     "client/src/pages/**/*.js",
-    "client/src/hooks/**/*.js"
+    "client/src/hooks/**/*.js",
+    "client/src/components/**/*.js"
   ],
   coverageThreshold: {
     global: {
