69ef88a4620492e60788af7ffee87014dd769d46
jyztintan <justintanwk2001@gmail.com>
2025-10-21 15:37:53 +0800

    fix: privacy policy image bug

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,27 +4,32 @@ import Layout from "./../components/Layout";
 const Policy = () => {
   return (
     <Layout title={"Privacy Policy"}>
-      <div className="row contactus align-items-center">
-        <div className="col-md-6 ">
-          <img
-            src="/images/contactus.jpeg"
-            alt="contactus"
-            style={{ width: "100%" }}
-          />
-        </div>
-        <div className="col-md-4 text-center">
-          <p>
-            At Virtual Vault, your privacy is important to us. We are dedicated to 
-            handling your personal information responsibly and transparently. This 
-            policy outlines the kinds of data we may collect when you use our website 
-            and how we use it to improve your experience.
-          </p>
-          <p>
-            By continuing to browse or use our services, you consent to the practices 
-            described in this policy. If you are not comfortable with these terms, we 
-            encourage you to discontinue use of our site. Protecting your trust is our 
-            priority, and we will always strive to keep your information secure.
-          </p>
+      <div className="container-fluid">
+        <h1 className="text-center bg-light p-3">Privacy Policy</h1>
+        <div className="row contactus align-items-center">
+          <div className="col-md-6 ">
+            <img
+              src="/images/privacy.jpeg"
+              alt="contactus"
+              style={{ width: "100%" }}
+            />
+          </div>
+          <div className="col-md-4 text-center">
+            <p>
+              At Virtual Vault, your privacy is important to us. We are
+              dedicated to handling your personal information responsibly and
+              transparently. This policy outlines the kinds of data we may
+              collect when you use our website and how we use it to improve your
+              experience.
+            </p>
+            <p>
+              By continuing to browse or use our services, you consent to the
+              practices described in this policy. If you are not comfortable
+              with these terms, we encourage you to discontinue use of our site.
+              Protecting your trust is our priority, and we will always strive
+              to keep your information secure.
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
